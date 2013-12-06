9215516f570e1cffb17cc2b8ddee5da40408aa9d
Andrew Ma <andrewma83@hotmail.com>
2013-12-05 22:05:14 -0800

    Checkin the backend files.

---
+++ memebot.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python
+# Created by:	Andrew Ma
+# Data:		11/28/2013
+# All Right Reserved	
+
+import urllib
+import os
+import re
+import MySQLdb
+from HTMLParser import HTMLParser
+
+
+db = ""
+cursor = ""
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    onclick = ""
+    href = ""
+    title = ""
+    episode_no = ""
+    part = ""
+    readyTitle = 0
+    episodeTitle = 0
+    episode = 0
+    part = 0
+    id = 0;
+    def handle_starttag(self, tag, attrs):
+	self.readyTitle = 0
+	if (tag == "h3"):
+	    for name, value in attrs:
+	    	if (name == "class" and value == "title"):
+		    self.readyTitle = 1
+		    self.onclick = ""
+		    self.href = ""
+		    self.title = ""
+		    self.part = self.part + 1
+		    break
+
+	if (tag == "h1"):
+	    for name, value in attrs:
+	    	if (name == "class" and value == "title"):
+		    self.episodeTitle = 1
+		    self.onclick = ""
+		    self.href = ""
+		    self.title = ""
+		    self.part = 0;
+		    break
+
+
+	if (tag == "a"):
+	    for name, value in attrs:
+	    	if (name == "href"):
+		    self.href = value
+		elif (name == "onclick"):
+		    value = re.sub('jsCapTraffic\(\'[ ]*', '', value);
+		    value = re.sub('[ ]+', '', value);
+		    value = re.sub('[\')]', '', value);
+		    value = re.sub(',', ' ', value);
+
+		    pattern = ur"[^ ]+[ ]+usdl"
+		    if (re.match(pattern, value)):
+		    	self.onclick = value
+			self.href = ""
+		    else:
+		     	self.onclick = ""
+
+
+		if (self.onclick != "" and self.href != ""):
+#		    print "    ", self.title, ":", self.href, ":", self.part, ":", self.id
+		    sql = "INSERT INTO program (id, episode, title, mp3_url, part) VALUES (%d, %d, '%s', '%s', %d);"\
+			   % (int(self.id), int(self.episode_no), self.title, self.href, self.part)
+		    
+		    try :
+			cursor.execute(sql)
+			db.commit()
+		    except:
+#		    	print "Need to rollback"
+		    	db.rollback()
+			break
+
+		    self.onclick = ""
+		    self.href = ""
+
+
+    def handle_data(self, data):
+	if (self.readyTitle == 1):
+	    self.title = data
+	    self.readyTitle = 0
+	if (self.episodeTitle == 1):
+	    pattern = ur"[ ]*[^ ]+[ ]+[^0-9]+([0-9]+)[^0-9]+"
+	    self.episode_no = re.search(pattern, data).group(1)
+#	    print "Expisode number: ", self.episode_no
+	    self.episodeTitle = 0
+
+
+def setup_db_conn ():
+    global db
+    global cursor
+    db = MySQLdb.connect("localhost", "root", "38RosIMU", "memehk")
+    cursor = db.cursor()
+    cursor.execute("set NAMES utf8;")
+    db.commit()
+
+def fetch_url (id):
+    url = "http://www3.memehk.com/index.php?page=program&action=alist&id=%s" % id
+    fd = urllib.urlopen(url)
+    s = fd.read()
+    return s
+
+
+def parse_html (data, id):
+    parser = MyHTMLParser()
+    parser.id = id
+    parser.feed(data)
+
+
+
+
+def memebot(id):
+    data = fetch_url(id)
+    setup_db_conn()
+    parse_html(data, id)
+    db.close()
+
+
+
+def main ():
+    for i in range(1, 11):
+	memebot(i) 
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ memehk.sql
@@ -0,0 +1,45 @@
+-- MySQL dump 10.13  Distrib 5.5.34, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: memehk
+-- ------------------------------------------------------
+-- Server version	5.5.34-0ubuntu0.12.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES big5 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `program`
+--
+
+DROP TABLE IF EXISTS `program`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `program` (
+  `id` int(11) NOT NULL,
+  `episode` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `mp3_url` varchar(100) NOT NULL,
+  `part` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`episode`,`part`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-12-05 22:02:58
